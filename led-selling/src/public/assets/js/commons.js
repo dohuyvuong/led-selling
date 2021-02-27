b71200c2998e26de08d1b41efa742ca43e717da8
Vuong Huy Do <dohuy.vuong@gmail.com>
2021-02-27 20:33:33 +0700

    fixed: scroll product images

--- led-selling/src/public/assets/js/commons.js
+++ led-selling/src/public/assets/js/commons.js
@@ -6,10 +6,29 @@ function showProductSpecifications() {
 }
 
 $(document).ready(function () {
+  let scrollCount = 0;
+  let direction = 0;
+
   $(".multiple-image-container").mousewheel(function(event, delta) {
-		this.scrollLeft -= (delta * 80);
-		this.scrollRight -= (delta * 80);
-		event.preventDefault();
+		const maxScrollable = $(".multiple-image-container")[0].scrollWidth - $(".multiple-image-container")[0].offsetWidth;
+
+    const canScrollToLeft = (this.scrollLeft >= 2) && (delta > 0);
+    const canScrollToRight = (this.scrollLeft <= (maxScrollable - 2)) && (delta < 0);
+    if (canScrollToLeft || canScrollToRight) {
+      this.scrollLeft -= (delta * 80);
+      event.preventDefault();
+      return;
+    }
+
+    if (direction != delta) {
+      scrollCount = 0;
+      direction = delta;
+    }
+
+    if (scrollCount < 3) {
+      scrollCount++;
+      event.preventDefault();
+    }
   });
 
   $(".product-card .product-image").tooltip({ html: true });
